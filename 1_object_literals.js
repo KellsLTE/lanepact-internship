e8ed19bb6bf972d5119893fe25b600c50bfc1139
Favour Max-Oti <maxotif@gmail.com>
2021-07-24 08:35:44 +0100

    initial commit

---
+++ 1_object_literals.js
@@ -0,0 +1,23 @@
+// const s = "Hello";
+// console.log(typeof s);
+
+// const s2 = new String('Hello');
+// console.log(typeof s2);
+
+// console.log(window);
+// alert(1);
+// console.log(navigator.appName);
+
+// Object Literal
+const book1 = {
+    title: 'Book One',
+    author: 'John Doe',
+    year: '2021',
+    getSummary: function() {
+        return `${this.title} was written by ${this.author} in ${this.year}`;
+    }
+};
+
+//console.log(book1.getSummary());
+//console.log(Object.values(book1));
+//console.log(Object.keys(book1));
